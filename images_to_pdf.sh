cf23fca38cbb190a3258c89b75d3f1851cc9fec6
Fred Jennings <sunistrimza+githublol@gmail.com>
2017-05-23 16:45:51 -0400

    Update images_to_pdf.sh

--- images_to_pdf.sh
+++ images_to_pdf.sh
@@ -1,8 +1,7 @@
-
 #!/bin/bash  
 # Asks for input directory where the PDFs to be merged are stored, asks for merged file name, outputs to folder
 # Requires imagemagick, runs in a staging folder for now
 
 shopt -s nullglob nocaseglob
-convert "$1"/*.{png,jpg,jpeg} "output.pdf"
+convert *.{png,jpg,jpeg} -quality 99 "output.pdf"
 exit
